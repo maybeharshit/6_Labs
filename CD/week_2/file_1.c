a792f29106f04704be95f842d31e58f725a3e43d
Harshit <nowyouseeme2203@gmail.com>
2026-01-12 18:07:47 +0530

    Test Files

---
+++ CD/week_2/file_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+#define VAR_1 1000
+#define VAR_2 200.50
+#define VAR_3 "abc"
+
+
+int main()
+{
+	int	a = VAR_1;
+	double	b = VAR_2;
+	char	c[10];
+	
+	strcpy(c,VAR_3);
+
+	return 0;
+}
